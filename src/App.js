b542eb845d595324db638e957d890f4808c75977
realkillfly <realkillfly@gmail.com>
2022-03-17 18:44:30 -0500

    Add deployment script

--- src/App.js
+++ src/App.js
@@ -1,52 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-import randomWord from './random-word-by-letter'
 
+import RandomWordComponent from './random-word-component'
 
-function makeWords()
-{
-  
-  var letters = 
-  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
-   'u', 'v', 'w', 'x', 'y', 'z'];
-
-  var startIx = Math.floor(Math.random() * (letters.length - 1 - 3)); //cannot start after 22 since we need the also get the next 3
-  var words = [];
-
-  
-  for (let index = 0; index < 4; index++) {
-    let letterIndex = startIx + index;
-    var nextWord = randomWord(letters[letterIndex]);
-    words.push(nextWord);
-  }
-
-  console.log("words is", words);
-  return words.map((w) =>    <h3 key={w}>{w}</h3>  );
-
-}
-
-function refresh(){
-  console.log("refreshing");
-  words = makeWords();
-}
-
-let words;
 
 function App() {
-
-  refresh();
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-        <button onClick={refresh}>  
-          Refresh
-        </button>
-        </div>
-          { words }
+        <p>
+          Written for a song prompt, this will generate four random english-language words
+          with consecutive starting letters.  For example a, b, c, d.
+        </p>
+        <RandomWordComponent />
       </header>
     </div>
   );
---
+++ src/random-word-component/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import randomWord from '../random-word-by-letter'
+
+export default class RandomWordComponent extends React.Component {
+
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            words: ''
+        }
+    }
+
+    componentDidMount()
+    {
+        this.makeWords();
+    }
+
+    makeWords = () => {
+
+        var letters =
+            ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y', 'z'];
+
+        var startIx = Math.floor(Math.random() * (letters.length - 1 - 3)); //cannot start after 22 since we need the also get the next 3
+        var words = [];
+
+
+        for (let index = 0; index < 4; index++) {
+            let letterIndex = startIx + index;
+            var nextWord = randomWord(letters[letterIndex]);
+            words.push(nextWord);
+        }
+
+        console.log("words is", words);
+        
+        this.setState({
+            words: words.map((w) => <h2 key={w}>{w}</h2>)
+        });
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h2><a href="#" class="App-link" onClick={this.makeWords}>REFRESH WORDS</a></h2>
+                { this.state.words }
+            </div>
+        );
+    }
+}
