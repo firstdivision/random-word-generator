57bb6d10b0b42c2d358b83a3fe741afec7a5ce36
realkillfly <realkillfly@gmail.com>
2022-03-17 17:35:33 -0500

    include word generator

---
+++ src/random-word-by-letter/letters/w.js
@@ -0,0 +1,304 @@
+const words = [
+  {
+    "word": "Worker"
+  },
+  {
+    "word": "Whisper"
+  },
+  {
+    "word": "Wire"
+  },
+  {
+    "word": "Whole"
+  },
+  {
+    "word": "Window"
+  },
+  {
+    "word": "Will"
+  },
+  {
+    "word": "Wait"
+  },
+  {
+    "word": "Wagon"
+  },
+  {
+    "word": "word"
+  },
+  {
+    "word": "Wash"
+  },
+  {
+    "word": "Winter"
+  },
+  {
+    "word": "Wriggle"
+  },
+  {
+    "word": "Widen"
+  },
+  {
+    "word": "Wonder"
+  },
+  {
+    "word": "Wear"
+  },
+  {
+    "word": "Wine"
+  },
+  {
+    "word": "Wreck"
+  },
+  {
+    "word": "Week"
+  },
+  {
+    "word": "Warn"
+  },
+  {
+    "word": "White"
+  },
+  {
+    "word": "Warrant"
+  },
+  {
+    "word": "Wave"
+  },
+  {
+    "word": "Witch"
+  },
+  {
+    "word": "Winner"
+  },
+  {
+    "word": "Willpower"
+  },
+  {
+    "word": "Well"
+  },
+  {
+    "word": "Weak"
+  },
+  {
+    "word": "Waist"
+  },
+  {
+    "word": "Worm"
+  },
+  {
+    "word": "World"
+  },
+  {
+    "word": "Waste"
+  },
+  {
+    "word": "Wrestle"
+  },
+  {
+    "word": "Waterfall"
+  },
+  {
+    "word": "Wording"
+  },
+  {
+    "word": "Wander"
+  },
+  {
+    "word": "Waiter"
+  },
+  {
+    "word": "Worry"
+  },
+  {
+    "word": "Welfare"
+  },
+  {
+    "word": "Wood"
+  },
+  {
+    "word": "Workshop"
+  },
+  {
+    "word": "Win"
+  },
+  {
+    "word": "Warning"
+  },
+  {
+    "word": "Walk"
+  },
+  {
+    "word": "Wardrobe"
+  },
+  {
+    "word": "Wild"
+  },
+  {
+    "word": "Wing"
+  },
+  {
+    "word": "Withdraw"
+  },
+  {
+    "word": "Wealth"
+  },
+  {
+    "word": "Wall"
+  },
+  {
+    "word": "Wedding"
+  },
+  {
+    "word": "Ward"
+  },
+  {
+    "word": "Wheel"
+  },
+  {
+    "word": "Welcome"
+  },
+  {
+    "word": "Wind"
+  },
+  {
+    "word": "Week"
+  },
+  {
+    "word": "Withdraw"
+  },
+  {
+    "word": "Wilderness"
+  },
+  {
+    "word": "Weigh"
+  },
+  {
+    "word": "Will"
+  },
+  {
+    "word": "West"
+  },
+  {
+    "word": "Widen"
+  },
+  {
+    "word": "Walk"
+  },
+  {
+    "word": "War"
+  },
+  {
+    "word": "Wrap"
+  },
+  {
+    "word": "Waterfall"
+  },
+  {
+    "word": "Want"
+  },
+  {
+    "word": "Wire"
+  },
+  {
+    "word": "Worry"
+  },
+  {
+    "word": "Work out"
+  },
+  {
+    "word": "Wear out"
+  },
+  {
+    "word": "Welfare"
+  },
+  {
+    "word": "Width"
+  },
+  {
+    "word": "Way"
+  },
+  {
+    "word": "Wreck"
+  },
+  {
+    "word": "Waste"
+  },
+  {
+    "word": "Whisper"
+  },
+  {
+    "word": "Witch"
+  },
+  {
+    "word": "Witness"
+  },
+  {
+    "word": "Work"
+  },
+  {
+    "word": "Wage"
+  },
+  {
+    "word": "Wall"
+  },
+  {
+    "word": "Worker"
+  },
+  {
+    "word": "Wine"
+  },
+  {
+    "word": "Wolf"
+  },
+  {
+    "word": "Wait"
+  },
+  {
+    "word": "Wound"
+  },
+  {
+    "word": "Water"
+  },
+  {
+    "word": "Winner"
+  },
+  {
+    "word": "Wriggle"
+  },
+  {
+    "word": "Wash"
+  },
+  {
+    "word": "Woman"
+  },
+  {
+    "word": "Warn"
+  },
+  {
+    "word": "Wake"
+  },
+  {
+    "word": "Worth"
+  },
+  {
+    "word": "Window"
+  },
+  {
+    "word": "Wrong"
+  },
+  {
+    "word": "Workshop"
+  },
+  {
+    "word": "Whole"
+  },
+  {
+    "word": "Wrist"
+  },
+  {
+    "word": "Wonder"
+  }
+ ];
+
+ export default words;
---
+++ src/random-word-by-letter/letters/z.js
@@ -0,0 +1,34 @@
+const words = [
+  {
+    "word": "Zone"
+  },
+  {
+    "word": "Zero"
+  },
+  {
+    "word": "Zags"
+  },
+  {
+    "word": "Zany"
+  },
+  {
+    "word": "Zaps"
+  },
+  {
+    "word": "Zarf"
+  },
+  {
+    "word": "Zeal"
+  },
+  {
+    "word": "Zebu"
+  },
+  {
+    "word": "Zeds"
+  },
+  {
+    "word": "Zees"
+  }
+ ];
+
+ export default words;
